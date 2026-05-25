c6deac134b8b63c41de11e4b02f6d3b701cd26dc
Guilherme Santos <161507384+Guilherme-Santos25@users.noreply.github.com>
2026-05-24 22:10:58 -0300

    Refactor SQL queries for better readability and structure

--- analises.sql
+++ analises.sql
@@ -28,3 +28,46 @@ join itens_pedido i on p.id_pedido = i.id_pedido group by c.nome, p.id_pedido or
 -- 8. Quais clientes gastaram mais de R$500 no total? Liste nome e valor total gasto.
 select c.nome, sum(pg.valor) as total_gasto from clientes c join pedidos p on c.id_cliente = p.id_cliente 
 join pagamentos pg on pg.id_pedido = p.id_pedido where pg.status = 'aprovado' group by c.nome having sum(pg.valor) > 500 order by total_gasto desc 
+
+-- 9. Liste todos os produtos com seu preço e a média de preço da sua categoria ao lado sem usar GROUP BY
+select nome_produto, preco, tipo_produto as categoria, 
+avg(preco) over(partition by tipo_produto) as media_valor_categoria 
+from produtos;
+
+-- 10. Qual o ranking dos clientes que mais gastaram? Use RANK() para numerar do maior para o menor
+--- primeiros vamos caucular o total gasto por cliente
+select c.nome, sum(pg.valor) as total_gasto from clientes c
+join pedidos p on c.id_cliente = p.id_cliente 
+join pagamentos pg on pg.id_pedido = p.id_pedido
+group by c.nome
+
+--- agora vamos transformar numa CTE (uma tabela temporário que eu posso utilizar como se fosse uma tabela normal)
+with total_por_cliente as (
+    select c.nome, sum(pg.valor) as total_gasto
+    from clientes c
+    join pedidos p on c.id_cliente = p.id_cliente
+    join pagamentos pg on pg.id_pedido = p.id_pedido
+    group by c.nome
+)
+select 
+    nome,
+    total_gasto,
+    rank() over (order by total_gasto desc) as ranking
+from total_por_cliente;
+
+-- 11. Quais pedidos tiveram valor acima da média geral de todos os pedidos?
+with valor_por_pedido AS (
+    select p.id_pedido, c.nome, sum(i.quantidade * i.preco_unitario) as valor_pedido
+    from pedidos p
+    join clientes c on c.id_cliente = p.id_cliente
+    join itens_pedido i on p.id_pedido = i.id_pedido
+    group by p.id_pedido, c.nome
+)
+select 
+    id_pedido,
+    nome,
+    valor_pedido,
+    round(avg(valor_pedido) over (), 2) as media_geral
+from valor_por_pedido
+where valor_pedido > (select avg(valor_pedido) from valor_por_pedido)
+order by valor_pedido desc;
